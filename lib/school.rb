78707636e124ca4faf0c7bda9dc803a97e36630e
Randi Schreiner <mimirandi21@gmail.com>
2020-10-12 15:57:18 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+    attr_reader :name
+
+    def initialize(name, roster= {})
+        @name = name
+        @roster = roster
+        
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if roster[grade] == nil
+            roster[grade] = []
+        end
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        return roster[grade]
+    end
+
+    def sort
+        return roster.each do |key, value|
+            roster[key] = value.sort{ |a,b| a<=>b }
+          end
+        
+    end
+        
+end
